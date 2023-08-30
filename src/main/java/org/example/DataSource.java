33178ecafaaec954b224856dc906fe606a881025
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-08-30 12:33:03 +0300

    AddFile: added bd into project to easy deploy

--- src/main/java/org/example/DataSource.java
+++ src/main/java/org/example/DataSource.java
@@ -1,12 +1,13 @@
 package org.example;
 
-import org.sqlite.SQLiteConfig;
 import org.sqlite.SQLiteDataSource;
 
 public class DataSource {
     public static SQLiteDataSource getDataSource(){
-        SQLiteDataSource ds=new SQLiteDataSource();
-        ds.setUrl("jdbc:sqlite:D:\\SQLite\\sqlite-tools-win32-x86-3420000\\SQLiteDataBasesCurExch.db");
+        SQLiteDataSource ds = new SQLiteDataSource();
+        // ds.setUrl("jdbc:sqlite:D:\\SQLite\\sqlite-tools-win32-x86-3420000\\SQLiteDataBasesCurExch.db");
+        ds.setUrl("jdbc:sqlite::resource:SQLiteDataBasesCurExch.db");
+
         return ds;
     }
 }
